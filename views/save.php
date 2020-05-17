f310bc790a3238b2706988ea1a853e9e5830feed
Mohsen <waltonmohsen@gmail.com>
2020-05-17 22:19:00 +0430

    init form save

--- views/save.php
+++ views/save.php
@@ -1,1 +1,14 @@
 <?php
+
+/**
+ * @param array $arg
+ */
+function orion_save_post_type($arg = array()){
+    wp_redirect(site_url());
+    if (isset($arg['customName'])){
+        echo $arg['customName'];
+    }
+}
+
+
+orion_save_post_type(array($_POST['customName']));
